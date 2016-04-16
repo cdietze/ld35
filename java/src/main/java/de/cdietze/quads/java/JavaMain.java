dcb70940562392f4d0450dc3a54fbdbb091455d0
Christoph Dietze <git@cdietze.de>
2016-04-16 09:30:34 +0200

    Initial skeleton commit

---
+++ java/src/main/java/de/cdietze/quads/java/JavaMain.java
@@ -0,0 +1,16 @@
+package de.cdietze.quads.java;
+
+import playn.java.LWJGLPlatform;
+
+import de.cdietze.quads.core.MainGame;
+
+public class JavaMain {
+
+  public static void main (String[] args) {
+    LWJGLPlatform.Config config = new LWJGLPlatform.Config();
+    // use config to customize the Java platform, if needed
+    LWJGLPlatform plat = new LWJGLPlatform(config);
+    new MainGame(plat);
+    plat.start();
+  }
+}
