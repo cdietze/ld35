c616e06ff714fb61cab5f1164ea3d80fb7b336ff
Christoph Dietze <christoph.dietze@gmail.com>
2016-04-16 18:06:20 +0200

    Read levels from external format

--- core/src/main/java/de/cdietze/quads/core/Level.java
+++ core/src/main/java/de/cdietze/quads/core/Level.java
@@ -1,10 +1,96 @@
 package de.cdietze.quads.core;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 import pythagoras.i.Dimension;
 import pythagoras.i.Rectangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Level {
-    public final Dimension dim = new Dimension(6, 6);
-    public final Rectangle rect = new Rectangle(dim);
-    public final int fieldCount = dim.width * dim.height;
+    public final Dimension dim;
+    public final Rectangle rect;
+    public final int fieldCount;
+    public final int playerStart;
+    //    public final List<Integer> blockers;
+    public final List<Integer> plainBlocks;
+    public final List<Integer> goals;
+
+    private Level(Builder builder) {
+        dim = builder.dim.clone();
+        rect = new Rectangle(dim);
+        fieldCount = dim.width * dim.height;
+        playerStart = builder.playerStart;
+        plainBlocks = ImmutableList.copyOf(builder.plainBlocks);
+        goals = ImmutableList.copyOf(builder.goals);
+    }
+
+    public static Level read(String s) {
+        Builder builder = new Builder();
+        List<String> rows = Splitter.on('\n').splitToList(s);
+        Dimension dim = new Dimension(rows.get(0).length(), rows.size());
+        rows.get(0).length();
+        for (int y = 0; y < rows.size(); y++) {
+            String row = rows.get(y);
+            for (int x = 0; x < row.length(); ++x) {
+                char c = row.charAt(x);
+                handleChar(c, PointUtils.toIndex(dim, x, y), builder);
+            }
+        }
+
+        builder.dim(dim);
+        return builder.build();
+    }
+
+    private static void handleChar(char c, int index, Builder builder) {
+        switch (c) {
+            case 'H':
+                builder.playerStart(index);
+                break;
+            case 'P':
+                builder.plainBlocks.add(index);
+                break;
+            default:
+              //  System.err.println("Warn: unknown char in level description: '" + c + "'");
+        }
+    }
+
+    public static final String lvl1 = "" +
+            ".h.....\n" +
+            "....P..\n" +
+            ".......\n" +
+            ".P.H...\n" +
+            ".......\n" +
+            ".......\n" +
+            "p..p...";
+
+    public static final Level level1 = read(lvl1);
+
+    public static final class Builder {
+        public Dimension dim;
+        public int playerStart;
+        public List<Integer> plainBlocks = new ArrayList<>();
+        public List<Integer> goals = new ArrayList<>();
+
+        public Builder() {}
+
+        public Builder dim(Dimension dim) {
+            this.dim = dim.clone();
+            return this;
+        }
+        public Builder playerStart(int val) {
+            playerStart = val;
+            return this;
+        }
+        public Builder plainBlocks(List<Integer> val) {
+            plainBlocks = val;
+            return this;
+        }
+        public Builder goals(List<Integer> val) {
+            goals = ImmutableList.copyOf(val);
+            return this;
+        }
+        public Level build() {return new Level(this);}
+    }
 }
